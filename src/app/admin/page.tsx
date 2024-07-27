c13746238295e5eaaef9551567d45da49d843669
abhishek <abhishekritu810219@gmail.com>
2024-07-28 02:49:43 +0530

    add sdhcn ui

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,10 +1,12 @@
 
 import React from 'react'
+import { Button } from '@/components/ui/button' 
 
 const page = () => {
   return (
     <>
-    <h1 className='text-3xl text-zinc-900 font-bold'>Admin Page</h1>      
+    <h1 className='text-3xl text-zinc-900 font-bold'>Admin Page</h1>   
+    <Button>Click Me</Button>   
     </>
   )
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,20 @@
 import { withAuth } from "next-auth/middleware"
 
 export default withAuth({
-   callbacks: {
-    authorized:({token,req}) =>{
-        console.log(token, 'token')
-        
-        if(req.nextUrl.pathname.startsWith('/admin')){
-            return token?.role === 'admin'
-        }else{
-            return true
+    callbacks: {
+        authorized: ({ token, req }) => {
+            console.log(token, 'token')
+
+            if (req.nextUrl.pathname.startsWith('/admin')) {
+                return token?.role === 'admin'
+            } else {
+                return true
+            }
         }
     }
-   }
 })
 
 export const config = {
-    matcher:["/admin(/.*)?",],
+    matcher: ["/admin(/.*)?",],
 }
 
